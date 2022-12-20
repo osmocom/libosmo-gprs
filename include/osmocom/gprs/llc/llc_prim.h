9f7a1428689b44a00c815c2a19f479077a40d246
Pau Espin Pedrol <pespin@sysmocom.de>
2022-12-21 04:02:18 +0700

    llc: split up enum osmo_gprs_llc_prim_type
    Change-Id: Ic5b7a2fb8d028a6e2031fb805189be4e92d80b9f
    Related: OS#5502

--- include/osmocom/gprs/llc/llc_prim.h
+++ include/osmocom/gprs/llc/llc_prim.h
@@ -24,31 +24,42 @@ static inline const char *osmo_gprs_llc_prim_sap_name(enum osmo_gprs_llc_prim_sa
 
 /* TS 04.64 Section 7.1.2 Table 7: LLC layer primitives (GMM/SNDCP/SMS/TOM) */
 /* TS 04.65 Section 5.1.2 Table 2: Service primitives used by SNDCP */
-enum osmo_gprs_llc_prim_type {
-	/* GMM <-> LLME */
-	OSMO_GPRS_LLC_LLGMM_ASSIGN,		/* Req: TLLI old, TLLI new, Kc, CiphAlg */
-	OSMO_GPRS_LLC_LLGMM_RESET,		/* Req/Cnf: TLLI */
-	OSMO_GPRS_LLC_LLGMM_TRIGGER,		/* Req: TLLI, Cause */
-	OSMO_GPRS_LLC_LLGMM_SUSPEND,		/* Req: TLLI, Page */
-	OSMO_GPRS_LLC_LLGMM_RESUME,		/* Req: TLLI */
-	OSMO_GPRS_LLC_LLGMM_PAGE,		/* Ind: TLLI */
-	OSMO_GPRS_LLC_LLGMM_IOV,		/* Req: TLLI */
-	OSMO_GPRS_LLC_LLGMM_STATUS,		/* Ind: TLLI, Cause */
-	OSMO_GPRS_LLC_LLGMM_PSHO,		/* Req/Ind/Cnf: TLLI, Ciph, IOV-UI, Old XID */
-	OSMO_GPRS_LLC_LLGMM_ASSIGN_UP,		/* Req: TLLI */
-	/* LLE <-> (GMM/SNDCP/SMS/TOM) */
-	OSMO_GPRS_LLC_LL_RESET,			/* Ind: TLLI */
-	OSMO_GPRS_LLC_LL_ESTABLISH,		/* Req/Ind/Rsp/Cnf: TLLI, XID Req/Neg, N201-I, N201-U */
-	OSMO_GPRS_LLC_LL_RELEASE,		/* Req/Ind/Cnf: TLLI, Local, Cause */
-	OSMO_GPRS_LLC_LL_XID,			/* Req/Ind/Rsp/Cnf: TLLI, XID Req/Neg, N201-I, N201-U */
-	OSMO_GPRS_LLC_LL_DATA,			/* Req/Ind/Cnf: TLLI, L3-PDU, Ref, QoS, Radio Prio */
-	OSMO_GPRS_LLC_LL_UNITDATA,		/* Req/Ind: TLLI, L3-PDU, QoS, Radio Prio, Ciph, ... */
-	OSMO_GPRS_LLC_LL_STATUS,		/* Ind: TLLI, Cause */
-	/* LLE <-> RLC/MAC (MS side) */
-	OSMO_GPRS_LLC_GRR_DATA,			/* Req/Ind: TLLI, LL-PDU, SAPI, Cause, QoS, Radio Prio */
-	OSMO_GPRS_LLC_GRR_UNITDATA,		/* Req/Ind: TLLI, LL-PDU, SAPI, QoS, Radio Prio */
-	/* LLE <-> BSSGP (SGSN side) */
-	OSMO_GPRS_LLC_BSSGP_UNITDATA,		/* Req/Ind: TLLI, LL-PDU, Cell Id, QoS, RLC Confirm, SAPI, ... */
+
+/* TS 04.65 Section 7.2.1 GMM - LLME primitives */
+enum osmo_gprs_llc_llgmm_prim_type {
+	OSMO_GPRS_LLC_LLGMM_ASSIGN,	/* Req: TLLI old, TLLI new, Kc, CiphAlg */
+	OSMO_GPRS_LLC_LLGMM_RESET,	/* Req/Cnf: TLLI */
+	OSMO_GPRS_LLC_LLGMM_TRIGGER,	/* Req: TLLI, Cause */
+	OSMO_GPRS_LLC_LLGMM_SUSPEND,	/* Req: TLLI, Page */
+	OSMO_GPRS_LLC_LLGMM_RESUME,	/* Req: TLLI */
+	OSMO_GPRS_LLC_LLGMM_PAGE,	/* Ind: TLLI */
+	OSMO_GPRS_LLC_LLGMM_IOV,	/* Req: TLLI */
+	OSMO_GPRS_LLC_LLGMM_STATUS,	/* Ind: TLLI, Cause */
+	OSMO_GPRS_LLC_LLGMM_PSHO,	/* Req/Ind/Cnf: TLLI, Ciph, IOV-UI, Old XID */
+	OSMO_GPRS_LLC_LLGMM_ASSIGN_UP,	/* Req: TLLI */
+};
+
+/* TS 04.65 Section 7.2.2 "Layer 3 - LLE primitives" */
+enum osmo_gprs_llc_ll_prim_type {
+	OSMO_GPRS_LLC_LL_RESET,		/* Ind: TLLI */
+	OSMO_GPRS_LLC_LL_ESTABLISH,	/* Req/Ind/Rsp/Cnf: TLLI, XID Req/Neg, N201-I, N201-U */
+	OSMO_GPRS_LLC_LL_RELEASE,	/* Req/Ind/Cnf: TLLI, Local, Cause */
+	OSMO_GPRS_LLC_LL_XID,		/* Req/Ind/Rsp/Cnf: TLLI, XID Req/Neg, N201-I, N201-U */
+	OSMO_GPRS_LLC_LL_DATA,		/* Req/Ind/Cnf: TLLI, L3-PDU, Ref, QoS, Radio Prio */
+	OSMO_GPRS_LLC_LL_UNITDATA,	/* Req/Ind: TLLI, L3-PDU, QoS, Radio Prio, Ciph, ... */
+	OSMO_GPRS_LLC_LL_STATUS,	/* Ind: TLLI, Cause */
+};
+
+/* TS 04.65 Section 7.2.3 "LLE - RLC/MAC primitives" (MS side) */
+enum osmo_gprs_llc_grr_prim_type {
+	OSMO_GPRS_LLC_GRR_DATA,		/* Req/Ind: TLLI, LL-PDU, SAPI, Cause, QoS, Radio Prio */
+	OSMO_GPRS_LLC_GRR_UNITDATA,	/* Req/Ind: TLLI, LL-PDU, SAPI, QoS, Radio Prio */
+};
+
+/* TS 04.65 Section 7.2.4 "LLE - BSSGP primitives" (SGSN side) */
+enum osmo_gprs_llc_bssgp_prim_type {
+	OSMO_GPRS_LLC_BSSGP_DL_UNITDATA,	/* Req: TLLI, LL-PDU, Cell Id, QoS, RLC Confirm, SAPI, ... */
+	OSMO_GPRS_LLC_BSSGP_UL_UNITDATA,	/* Ind: TLLI, LL-PDU, Cell Id, edirect attempt, IMSI, V(U) for redirect, ... */
 };
 
 /* Parameters for OSMO_GPRS_LLC_LLGMM_* prims */
